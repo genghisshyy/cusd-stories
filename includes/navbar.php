f3775895190ca00094b8d9ce57db641b25a05335
jc2747 <jc2747@cornell.edu>
2018-03-18 16:48:03 -0400

    navbar and footer-jintana

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,15 @@
 <!-- consistent navbar here (do something to make it show the active tag) -->
-<div class="navbar"> 
-<p> I am navbar</p>
+<div class="navbar">
+
+ <div class = "left">
+
+<a class = "logo" href = "index.php">
+<img class ="cusd-logo" src = "img/greyscaled-cusd-logo.svg">
+Design</a>
+
 </div>
 
+<a href = "index.php"> Articles</a>
+
+<a href = "index.php"> Videos</a>
+</div>
