d51e298ca75e76e6b260c1147fa6245d7c322669
jc2747 <jc2747@cornell.edu>
2018-03-18 17:23:21 -0400

    grid setup

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,5 @@
 <!-- consistent navbar here (do something to make it show the active tag) -->
+<!-- <div class="nav-wrapper">
 <div class="navbar">
 
  <div class = "left">
@@ -13,3 +14,15 @@ Design</a>
 
 <a href = "index.php"> Videos</a>
 </div>
+</div> -->
+
+
+<nav>
+    <div class="nav-wrapper">
+      <a href="" class="brand-logo">Design</a><img class ="cusd-logo" src = "img/greyscaled-cusd-logo.svg">
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="">Articles</a></li>
+        <li><a href="">Videos</a></li>
+      </ul>
+    </div>
+  </nav>
