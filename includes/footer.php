9f8a3264bf4020683f9bb75a551409f33c32cdd0
jc2747 <jc2747@cornell.edu>
2018-03-18 21:39:36 -0400

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
-<div class="footer">
+<footer>
 
 <p class = "left"><a class = "footer-link" href ="https://www.facebook.com/CUSustainableDesign/">Facebook</a> <a class = "footer-link" href ="https://www.cusd.cornell.edu">Website</a> <a class = "footer-link" href ="">Instagram</a> </p>
 
 
  <p class = "right">
 <span>&copy;&nbsp;2011&ndash;2018 Cornell University Sustainable Design</span></p>
-</div>
+</footer>
