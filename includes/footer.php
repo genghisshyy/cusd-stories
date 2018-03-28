a6dec483267b69bc78d263fe505a29acdb700e26
jc2747 <jc2747@cornell.edu>
2018-03-28 18:36:13 -0400

    i think it oke now

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,6 @@
 <p class = "float-left"><a class = "footer-link" href ="https://www.facebook.com/CUSustainableDesign/">Facebook</a> <a class = "footer-link" href ="https://www.cusd.cornell.edu">Website</a> <a class = "footer-link" href ="https://www.instagram.com">Instagram</a> </p>
 
 
- <p class = "right">
-<span style="margin-right:28px">&copy;&nbsp;2011&ndash;2018 Cornell University Sustainable Design</span></p>
+ <p class = "float-right">
+&copy;&nbsp;2011&ndash;2018 Cornell University Sustainable Design</p>
 </footer>
