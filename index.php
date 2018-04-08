e806dd654e878a8e577f38426e72456736362b3e
Greg Schultz <gjs86@cornell.edu>
2018-04-08 01:05:05 -0400

    welcome message

--- index.php
+++ index.php
@@ -18,6 +18,10 @@
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
     <?php include "includes/navbar.php"; ?>
+    <div class ="row center-align welcome">
+      <h2> What do our members do? </h2>
+      <p> A place to see what the members of CUSD do in their academic and professional lives </p>
+    </div>
     <div class="row center-align">
        <?php include "includes/allLoad.php"; ?>
     </div>
